e5abeb45eebc4ef37fb5787c160437c3ec091932
dia2209 <diamanandhar1@gmail.com>
2025-04-19 18:33:15 +0545

    Add files via upload

--- cie4.c
+++ cie4.c
@@ -1,22 +1,36 @@
 //Name: Diya Manandhar   Class:BCSIT-A
-//25.Write a function to count the number of words in a given string
 
-#include <stdio.h>
+//2.Write a C program to find the largest and smallest element in an array.
 
-int count_words(char str[]) {
-    int count = 0;
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0')) {
-            count++;
+#include<stdio.h>
+int main(){
+    int N;
+    printf("Enter the number of elements in the array: ");
+    scanf("%d",&N);
+
+    int arr[N];
+    printf("Enter %d number for the array: ",N);
+    
+
+    for(int i=0;i<N;i++){
+        scanf("%d",&arr[i]);
+    }
+
+    int max=arr[0];
+    int min=arr[0];
+
+    for(int i=1;i<N;i++){//1 because 0 is already max and min
+        if(max<arr[i]){
+            max=arr[i];
+        }
+        if(min>arr[i]){
+            min=arr[i];
         }
     }
-    return count;
-}
-
-int main() {
-    char str[100];
-    printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin); // Reads input with spaces
-    printf("Number of words: %d\n", count_words(str));
+       
+    printf("The largest element is %d\n",max);
+    printf("The smallest element is %d\n",min);
     return 0;
-}
+    }
+    
+
